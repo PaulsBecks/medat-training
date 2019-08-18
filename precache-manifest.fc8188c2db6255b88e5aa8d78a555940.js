8a2c56519c586d99013d0a0c124958562519806a
PaulsBecks <paul.beck.berlin@gmail.com>
2019-08-18 21:52:38 +0200

    Updates

---
+++ precache-manifest.fc8188c2db6255b88e5aa8d78a555940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56bba4412a88b61f157045b308b8bfc1",
+    "url": "/medat-training/index.html"
+  },
+  {
+    "revision": "c89901741deee4b8157b",
+    "url": "/medat-training/static/css/main.25922f1a.chunk.css"
+  },
+  {
+    "revision": "f352b9380448be8af6fa",
+    "url": "/medat-training/static/js/2.d698f0be.chunk.js"
+  },
+  {
+    "revision": "c89901741deee4b8157b",
+    "url": "/medat-training/static/js/main.39245cba.chunk.js"
+  },
+  {
+    "revision": "28061208dda1e67b7916",
+    "url": "/medat-training/static/js/runtime~main.63ac7767.js"
+  }
+]);
